986aae8726e27f01d74519ef8f3de3036934fcc6
24KVenus <guoyijie_george@163.com>
2017-11-14 23:07:39 +0800

    Create New Project

---
+++ Sample_Swift/Sample_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample_Swift
+//
+//  Created by 郭 奕杰 on 2017/11/14.
+//  Copyright © 2017年 GYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
